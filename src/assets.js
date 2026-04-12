ff075391188b0c743e1a565d72f3c3413a5f6522
“chenlong” <979877240@qq.com>
2026-04-12 16:35:25 +0800

    perf(assets): 将图片格式从png/jpg转换为webp以优化性能

--- src/assets.js
+++ src/assets.js
@@ -1,11 +1,11 @@
 const IMAGE_MAP = {
-  OG8K: 'sbti/OJBK.png',
-  'FU?K': 'sbti/FUCK.png',
-  'Dior-s': 'sbti/Dior-s.jpg',
-  'JOKE-R': 'sbti/JOKE-R.jpg',
+  OG8K: 'sbti/OJBK.webp',
+  'FU?K': 'sbti/FUCK.webp',
+  'Dior-s': 'sbti/Dior-s.webp',
+  'JOKE-R': 'sbti/JOKE-R.webp',
 }
 
 export function getImageUrl(code) {
-  const filename = IMAGE_MAP[code] || `sbti/${code}.png`
+  const filename = IMAGE_MAP[code] || `sbti/${code}.webp`
   return new URL(filename, new URL(import.meta.env.BASE_URL, window.location.origin)).href
 }
